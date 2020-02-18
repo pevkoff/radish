798425a2ba7fe7bb713b811ad3089b7b944c63d1
chebyrash <pd.stroganov@gmail.com>
2020-02-18 00:30:48 +0000

    First working impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use core::time::Duration;
+use bytes::buf::BufExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use serde::{Deserialize, Serialize};
@@ -6,50 +6,71 @@ use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime};
 
-#[derive(Debug, Copy, Clone)]
-struct Node<'a> {
-    value: &'a str,
-    ttl: Option<Duration>,
+fn get_timestamp() -> u128 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
 }
 
-impl<'a> Node<'a> {
-    fn new(value: &'a str, ttl: Option<Duration>) -> Self {
+fn calc_expiration(ttl: Duration) -> u128 {
+    get_timestamp() + ttl.as_millis()
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    value: String,
+    expiration: Option<u128>,
+}
+
+impl Node {
+    fn new(value: String, expiration: Option<u128>) -> Self {
         Self {
             value: value,
-            ttl: ttl,
+            expiration: expiration,
         }
     }
 }
 
 #[derive(Debug, Clone)]
-struct SafeMap<'a> {
-    underlying: Arc<Mutex<HashMap<&'a str, Node<'a>>>>,
+struct SafeMap {
+    underlying: Arc<Mutex<HashMap<String, Node>>>,
+    auto_evict: bool,
 }
 
-impl<'a> SafeMap<'a> {
+impl SafeMap {
     fn new(auto_evict: bool) -> Self {
-        let map = Self {
+        Self {
             underlying: Arc::new(Mutex::new(HashMap::new())),
-        };
-
-        if !auto_evict {
-            return map;
+            auto_evict: auto_evict,
         }
-
-        map
     }
 
-    fn get<'temp>(self, key: &'temp str) -> Option<&'a str> {
-        let map = self.underlying.lock().unwrap();
+    fn get(self, key: String) -> Option<String> {
+        let mut map = self.underlying.lock().unwrap();
+        let key = key.as_str();
+
         match map.get(key) {
-            Some(node) => Some(node.value),
-            None => None,
+            Some(node) => {
+                if let Some(expiration) = node.expiration {
+                    if get_timestamp() > expiration {
+                        map.remove(key);
+                        return None;
+                    }
+                }
+                Some(node.value.clone())
+            }
+            _ => None,
         }
     }
 
-    fn set(&self, key: &'a str, value: &'a str) -> Option<&'a str> {
-        let node = Node::new(value, None);
+    fn set(self, key: String, value: String, ttl: Option<Duration>) -> Option<String> {
+        let node = Node::new(
+            value.clone(),
+            ttl.map_or(None, |ttl| Some(calc_expiration(ttl))),
+        );
         let mut map = self.underlying.lock().unwrap();
 
         map.insert(key, node);
@@ -57,58 +78,82 @@ impl<'a> SafeMap<'a> {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct GetRequest {
-    key: String,
-}
-#[derive(Serialize, Deserialize)]
-struct SetRequest {
-    key: String,
-    value: String,
+#[derive(Serialize, Deserialize, Debug)]
+struct RadishResponse {
+    query: String,
+    data: Option<String>,
 }
 
-async fn handler(req: Request<Body>, map: SafeMap<'_>) -> Result<Response<Body>, Infallible> {
+impl RadishResponse {
+    fn new(query: String, data: Option<String>) -> Self {
+        Self {
+            query: query,
+            data: data,
+        }
+    }
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct RadishSetRequest {
+    value: String,
+    ttl: u64,
+}
+
+async fn handler(req: Request<Body>, map: SafeMap) -> Result<Response<Body>, Infallible> {
     if req.uri().path() == "/" {
         return Ok(Response::new(Body::from("Index Page")));
     }
-
-    match req.method() {
-        &Method::GET => {
-            let key = req.uri().path();
-
-            match map.get(key) {
-                Some(value) => Ok(Response::new(Body::from(value))),
-                None => Ok(Response::new(Body::from("null"))),
-            }
-        }
-        &Method::POST => {
-            let key = req.uri().path();
-
-            match map.set(key, key) {
-                Some(value) => Ok(Response::new(Body::from(value))),
-                None => Ok(Response::new(Body::from("null"))),
-            }
-        }
-        _ => Ok(Response::builder()
+    if req.method() != &Method::GET && req.method() != &Method::POST {
+        return Ok(Response::builder()
             .status(StatusCode::METHOD_NOT_ALLOWED)
             .body(Body::from("Method Not Allowed"))
-            .unwrap()),
+            .unwrap());
     }
+
+    let mut path = req.uri().path().to_string();
+    path.remove(0);
+    let key = path;
+
+    let data = match req.method() {
+        &Method::GET => map.get(key.clone()),
+        &Method::POST => {
+            let body = (hyper::body::aggregate(req).await).unwrap();
+            let r: RadishSetRequest = serde_json::from_reader(body.reader()).unwrap();
+            map.set(key.clone(), r.value, Some(Duration::from_secs(r.ttl)))
+        }
+        _ => None,
+    };
+    let response = RadishResponse::new(key.clone(), data);
+    let serialized = serde_json::to_string(&response).unwrap();
+
+    Ok(Response::builder()
+        .header("Content-Type", "application/json")
+        .status(StatusCode::OK)
+        .body(Body::from(serialized))
+        .unwrap())
 }
 
 #[tokio::main]
 pub async fn main() {
-    let safe_map = SafeMap::new(false);
+    let safe_map = SafeMap::new(true);
+
+    // Это надо перенести в функцию SafeMap
+    let cln = safe_map.clone();
+    tokio::spawn(async move {
+        loop {
+            let map = cln.clone();
+            tokio::time::delay_for(Duration::from_secs(5)).await;
+
+            let now = get_timestamp();
+            map.underlying
+                .lock()
+                .unwrap()
+                .retain(|_, v| v.expiration.map_or(true, |exp| exp > now));
+        }
+    });
 
     let service = make_service_fn(move |_| {
         let map = safe_map.clone();
-
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                println!("{}", "Processing new req");
-                handler(req, map.clone())
-            }))
-        }
+        async move { Ok::<_, Infallible>(service_fn(move |req| handler(req, map.clone()))) }
     });
 
     let server = Server::bind(&SocketAddr::from(([0, 0, 0, 0], 5000))).serve(service);
